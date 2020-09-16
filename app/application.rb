9b1ce46282e41cb461af50196476e349719e7bed
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-09-16 21:32:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,7 @@ class Application
 
       if req.path.match(/items/)
         item_name = req.path.split("/items/").last
+      end
         if item =@@items.find{|i| i.name == item_name}
           resp.write item.price
         else
