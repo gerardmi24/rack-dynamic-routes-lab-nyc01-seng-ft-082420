dd3ae66eb46d9b842d75ff33c94ed329ab3a1bbf
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-09-16 21:17:45 +0000

    Automatically backed up by Learn

--- app/item.rb
+++ app/item.rb
@@ -6,16 +6,4 @@ class Item
     @price = price
   end
 
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Response.new(env)
-
-    if req.path=="/items"
-      resp.write "Here are your items"
-    elsif resp.write "Route not found"
-      resp.status = 404
-    end
-    resp.finish
-  end
-
-end
+  
