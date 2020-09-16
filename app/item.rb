bfbadaa5add99485d5fec1c548432dcb2e62fb36
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-09-16 21:13:15 +0000

    Automatically backed up by Learn

--- app/item.rb
+++ app/item.rb
@@ -4,5 +4,17 @@ class Item
   def initialize(name,price)
     @name = name
     @price = price
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Response.new(env)
+
+    if req.path=="/songs"
+      resp.write "You requested the songs"
+    elsif resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
   end
+
 end
